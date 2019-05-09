a6b18c0fdc73b27d0fe4461eaa03fb48f4c21390
abhishekkundu <akundu@calpoly.edu>
2019-05-09 15:20:12 -0700

    Adding Font

---
+++ app/src/main/java/edu/calpoly/clubng/CreateAcc.java
@@ -0,0 +1,12 @@
+package edu.calpoly.clubng;
+import  android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAcc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
