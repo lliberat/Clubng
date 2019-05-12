b6beea642ea641eeff1661eb8c9e61a01d453331
abhishekkundu <akundu@calpoly.edu>
2019-05-12 13:07:54 -0700

    Profile Updated XML

--- app/src/main/java/edu/calpoly/clubng/MainActivity.java
+++ app/src/main/java/edu/calpoly/clubng/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.calpoly.clubng;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomnav = findViewById(R.id.footer_nav);
+        bottomnav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                new LocationFragment()).commit(); //sets default fragment
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFrag = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_profile:
+                            selectedFrag = new ProfileFragment();
+                            break;
+                        case R.id.nav_location:
+                            selectedFrag = new LocationFragment();
+                            break;
+                        case R.id.nav_confirmation:
+                            selectedFrag = new ConfirmationFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                            selectedFrag).commit();
+                    return true;
+                }
+            };
 }
