2e436c8c910bf1b8869fa59791c86c2833035951
abhishekkundu <akundu@calpoly.edu>
2019-06-02 16:15:29 -0700

    Notifications Enabled

--- app/src/main/java/edu/calpoly/clubng/PaymentController.java
+++ app/src/main/java/edu/calpoly/clubng/PaymentController.java
@@ -1,5 +1,8 @@
 package edu.calpoly.clubng;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 
 public class PaymentController extends AppCompatActivity {
 
@@ -20,6 +24,8 @@ public class PaymentController extends AppCompatActivity {
         assert actionBar != null;
 
         actionBar.setDisplayHomeAsUpEnabled(true);
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
         Button PayButton = findViewById(R.id.button5);
         PayButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +35,30 @@ public class PaymentController extends AppCompatActivity {
         });
     }
     public void openMainActivity(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
+        Intent intent = new Intent(this, NewMessageNotification.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("CONGRATULATIONS!")
+                .setContentText("You are going CLUBNG")
+                .setColor(getTitleColor())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+               .build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
+
     }
 
     @Override
