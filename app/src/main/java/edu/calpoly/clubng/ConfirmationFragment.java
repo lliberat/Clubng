9a01ec74417ce02d1bdf2f58471ce859d59680f1
abhishekkundu <akundu@calpoly.edu>
2019-06-12 17:06:30 -0700

    Added Conformation Page Changes. Developed UI and Controller
    DONE WITH THE PROJECT.
    HAVE A GREAT SUMMER! THIS WAS A GREAT CLASS :)

--- app/src/main/java/edu/calpoly/clubng/ConfirmationFragment.java
+++ app/src/main/java/edu/calpoly/clubng/ConfirmationFragment.java
@@ -7,11 +7,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ConfirmationFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_confirmation,container,false);
+        final Globals g = Globals.getInstance();
+        View view = inflater.inflate(R.layout.fragment_confirmation,container,false);
+        RelativeLayout rl = (RelativeLayout) view.findViewById(R.id.nav_confirmation);
+        TextView artist = view.findViewById(R.id.ArtistName);
+        TextView club = view.findViewById(R.id.Club);
+        TextView city = view.findViewById(R.id.City);
+        TextView date = view.findViewById(R.id.Date);
+        TextView statement = view.findViewById(R.id.Statement);
+        if(g.getEvent()!=null){
+            Event event = g.getEvent();
+            artist.setText(event.getArtist());
+            club.setText(event.getClub());
+            city.setText(event.getCity());
+            date.setText(event.getDay()+". "+event.getMonth()+". "+event.getYear());
+            statement.setText("You are going Clubng");
+        }else {
+            statement.setText("Lets Go Clubng");
+        }
+
+        return view;
+
     }
 }
